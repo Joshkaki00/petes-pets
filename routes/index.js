42473d3117b8b0365db5113a5f18f302a3356226
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-09-05 15:52:58 -0700

    Refactor pet fetching logic to use promises for better error handling

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const Pet = require('../models/pet');
 
 module.exports = (app) => {
-
   /* GET home page. */
   app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
-    });
+    Pet.find()
+      .then(pets => {
+        res.render('pets-index', { pets: pets });
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send('Error fetching pets');
+      });
   });
 }
